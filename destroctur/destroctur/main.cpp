9cae38b2e82c44955a1596caa5bd7ca5272522ca
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-09 00:10:42 +0800

    Add files via upload

---
+++ destroctur/destroctur/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  destroctur
+//
+//  Created by Nisha Ramprasath on 8/11/23.
+//
+
+#include<iostream>
+using namespace std;
+    
+class Demo
+{
+    int *p;
+    public:
+    Demo()
+    {
+        p=new int[10];
+        cout<<"Constructor of Demo"<<endl;
+    }
+            
+    ~Demo()
+    {
+        delete[]p;
+           cout<<"Destructor of Demo"<<endl;
+    }
+    
+};
+    
+void fun()
+{
+    Demo *p=new Demo();
+    delete p;
+}
+    
+int main()
+{
+    fun();
+}
+    
